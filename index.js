83f1555a4dbf46a956364bff55449136d023c3e3
Keitel Jovin <keiteldog@gmail.com>
2024-05-06 16:45:55 -0400

    Fix public static file serving

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.set('trust proxy', true);
 app.use(cors());
 app.use(express.json({ limit:'2mb' }));
 app.use(express.urlencoded({ limit:'1mb', extended: true, parameterLimit: 50 }));
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 function getDateStr () {
   const date = new Date();
